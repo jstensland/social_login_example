59648fa258e3739d13772daa603f5481bae26e60
Jared Stensland <jared.stensland@gmail.com>
2015-06-09 22:28:07 -0700

    include Janrain Social Media Login on the home page.  add other javascript experiement

---
+++ app/assets/javascripts/janrain_login.js
@@ -0,0 +1,26 @@
+(function() {
+    if (typeof window.janrain !== 'object') window.janrain = {};
+    if (typeof window.janrain.settings !== 'object') window.janrain.settings = {};
+
+    janrain.settings.tokenUrl = '__REPLACE_WITH_YOUR_TOKEN_URL__';
+
+    function isReady() { janrain.ready = true; }
+    if (document.addEventListener) {
+      document.addEventListener("DOMContentLoaded", isReady, false);
+    } else {
+      window.attachEvent('onload', isReady);
+    }
+
+    var e = document.createElement('script');
+    e.type = 'text/javascript';
+    e.id = 'janrainAuthWidget';
+
+    if (document.location.protocol === 'https:') {
+      e.src = 'https://rpxnow.com/js/lib/social-login-example/engage.js';
+    } else {
+      e.src = 'http://widget-cdn.rpxnow.com/js/lib/social-login-example/engage.js';
+    }
+
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(e, s);
+})();
