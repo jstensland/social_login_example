a31511cb3aa2d93939c99c639d77222ffc4f9668
Jared Stensland <jstensland@janrain.com>
2015-10-09 14:54:19 -0700

    up version of ruby and add social sharing bar to the page

---
+++ app/assets/javascripts/static_pages.js
@@ -0,0 +1,25 @@
+// # Place all the behaviors and hooks related to the matching controller here.
+// # All this logic will automatically be available in application.js.
+// # You can use CoffeeScript in this file: http://coffeescript.org/
+
+
+$.externalScript("http://cdn-social.janrain.com/social/janrain-social.min.js")
+// # settings for the app
+janrain.settings.appUrl = 'https://social-login-example.rpxnow.com';
+janrain.settings.social = {
+   providers: [
+       "facebook",
+       "twitter",
+       "native-facebook",
+       "native-googleplus"
+   ],
+   orientation: "vertical",
+   formFactor: "drawer"
+};
+
+
+window.addEventListener("load", pageFullyLoaded, false);
+
+function pageFullyLoaded(e) {
+   janrain.social.addWidgetTo($('.janrainSocialPlaceholder').get(0));
+};
